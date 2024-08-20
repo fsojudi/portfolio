b7aaff4e594c0aa276ef2ca6fd1f4592552e6544
fsojudi <fsojudi@gmail.com>
2024-08-20 17:48:59 +0200

    creat sample style and script

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple script to handle smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
